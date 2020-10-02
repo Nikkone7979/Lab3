60e462e967d8dd344c2175b4c864ba1234e303db
Youness Elwiddi <nikkone7979@gmail.com>
2020-10-02 17:21:09 -0400

    Update C#QuickSort.cs

--- C#QuickSort.cs
+++ C#QuickSort.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-public class Bubble_Sort
+public class BubbleSort
 {
     public static void Main(string[] args)
     {
         int[] a = { 1, 3, 5, 6, 2, 3, 5 };
         int j;
-        for (int p = 0; p <= a.Length - 2; p++)
-        {
-            for (int i = 0; i <= a.Length - 2; i++)
-            {
-                if (a[i] > a[i + 1])
-                {
+        for (int p = 0; p <= a.Length - 2; p++){
+            for (int i = 0; i <= a.Length - 2; i++){
+                if (a[i] > a[i + 1]){
                     j = a[i + 1];
                     a[i + 1] = a[i];
                     a[i] = j;
@@ -20,7 +17,6 @@ public class Bubble_Sort
         Console.WriteLine("\n" + "Sorted numbers: ");
         foreach (int arraydisplay in a)
             Console.Write(arraydisplay + " ");
-        Console.Write("\n");
 
     }
 }
