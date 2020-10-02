2ad6ef5ce4067e4b6de025cf2743e4330b9bd272
Youness Elwiddi <nikkone7979@gmail.com>
2020-10-02 11:41:38 -0400

    Writing imperatively in C#

---
+++ C#QuickSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Bubble_Sort
+{
+    public static void Main(string[] args)
+    {
+        int[] a = { 1, 3, 5, 6, 2, 3, 5 };
+        int j;
+        for (int p = 0; p <= a.Length - 2; p++)
+        {
+            for (int i = 0; i <= a.Length - 2; i++)
+            {
+                if (a[i] > a[i + 1])
+                {
+                    j = a[i + 1];
+                    a[i + 1] = a[i];
+                    a[i] = j;
+                }
+            }
+        }
+        Console.WriteLine("\n" + "Sorted numbers: ");
+        foreach (int arraydisplay in a)
+            Console.Write(arraydisplay + " ");
+        Console.Write("\n");
+
+    }
+}
